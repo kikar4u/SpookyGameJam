3c0ceb854588eac4032443cda44d51ecfd51b376
Sylve <kikar4u@gmail.com>
2021-10-23 13:20:10 +0200

    [CLEANUP][FEATURE]Begin hide features on player

--- Assets/Script/PlayerController/PlayerController.cs
+++ Assets/Script/PlayerController/PlayerController.cs
@@ -10,14 +10,33 @@ public class PlayerController : MonoBehaviour
     float vertical;
     float horizontal;
     bool hiding = false;
+    bool hidden = false;
+    int lastPressed = 0;
     Rigidbody2D body;
+    GameObject currentHideout;
+    Vector3 bufferPosition;
     // Start is called before the first frame update
     void Start()
     {
         speedBuffer = speed;
         body = GetComponent<Rigidbody2D>();
     }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && hiding && currentHideout != null)
+        {
+            Debug.Log("premier if");
+            //Debug.Log("veut rentrer");
+            getHidden(true);
+            lastPressed++;
 
+        }
+        if (Input.GetButtonDown("Fire1") && !hiding && lastPressed == 1)
+        {
+            Debug.Log("deuxième if");
+        }
+
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -30,23 +49,58 @@ public class PlayerController : MonoBehaviour
             speed = 10.0f;
             //Debug.Log("i am speed" + speed);
         }
-        else
+        else if(!hiding)
         {
             speed = speedBuffer;
             //Debug.Log("speed goes back");
         }
+
+
+
+
+
+
+
     }
-    void getHidden()
+    void getHidden(bool isNotHidden)
     {
-        hiding = true;
+
+        if(isNotHidden){
+            bufferPosition = gameObject.GetComponent<Transform>().position;
+            speed = 0;
+            currentHideout.GetComponent<BoxCollider2D>().isTrigger = true;
+            Debug.Log(bufferPosition);
+            gameObject.GetComponent<Transform>().position = currentHideout.gameObject.GetComponent<Transform>().position;
+            hidden = true;
+            hiding = false;
+        }
+        else
+        {
+            Debug.Log(currentHideout);
+            speed = speedBuffer;
+            gameObject.GetComponent<Transform>().position = bufferPosition;
+            currentHideout.GetComponent<BoxCollider2D>().isTrigger = false;
+
+        }
+
     }
-    void OnTriggerEnter2D(Collider2D col)
+    void OnCollisionEnter2D(Collision2D col)
     {
         if(col.gameObject.tag == "hideout")
         {
-            Debug.Log("OnCollisionEnter2D");
+            //Debug.Log("HIDE" + col.gameObject.GetComponent<Transform>().position);
+            hiding = true;
+            currentHideout = col.gameObject;
         }
 
 
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "hideout")
+        {
+            hiding = false;
+
+        }
     }
 }
