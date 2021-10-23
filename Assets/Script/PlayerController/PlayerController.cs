9a1cb3821d8984db457b1ea02272d865d40f41ab
leoBG <leobgely@gmail.com>
2021-10-23 13:34:57 +0200

    Sanisation Code

--- Assets/Script/PlayerController/PlayerController.cs
+++ Assets/Script/PlayerController/PlayerController.cs
@@ -5,40 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 1.0f;
-    float speedBuffer;
-    float vertical;
-    float horizontal;
-    bool hiding = false;
-    bool hidden = false;
-    int lastPressed = 0;
-    Rigidbody2D body;
-    GameObject currentHideout;
-    Vector3 bufferPosition;
+    [SerializeField] private float speed = 1.0f;
+    private float speedBuffer;
+    private float vertical;
+    private float horizontal;
+    private bool isHiding;
+    private bool hidden; //Jamais utilisé pour le moment, peut être supprimé dans l'état
+    private int lastPressed;
+    [SerializeField] private Rigidbody2D body;
+    private GameObject currentHideout;
+    private Vector3 bufferPosition;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         speedBuffer = speed;
-        body = GetComponent<Rigidbody2D>();
     }
-    void Update()
+    private void Update()
     {
-        if (Input.GetButtonDown("Fire1") && hiding && currentHideout != null)
+        if (Input.GetButtonDown("Fire1") && isHiding && currentHideout != null)
         {
             Debug.Log("premier if");
             //Debug.Log("veut rentrer");
-            getHidden(true);
+            GetHidden(true);
             lastPressed++;
 
         }
-        if (Input.GetButtonDown("Fire1") && !hiding && lastPressed == 1)
+        if (Input.GetButtonDown("Fire1") && !isHiding && lastPressed == 1)
         {
-            Debug.Log("deuxi�me if");
+            Debug.Log("deuxi�me if");
         }
 
     }
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
 
         horizontal = Input.GetAxis("Horizontal");
@@ -49,58 +49,51 @@ public class PlayerController : MonoBehaviour
             speed = 10.0f;
             //Debug.Log("i am speed" + speed);
         }
-        else if(!hiding)
+        else if(!isHiding)
         {
             speed = speedBuffer;
             //Debug.Log("speed goes back");
         }
-
-
-
-
-
-
-
     }
-    void getHidden(bool isNotHidden)
+    private void GetHidden(bool isNotHidden)
     {
 
-        if(isNotHidden){
-            bufferPosition = gameObject.GetComponent<Transform>().position;
+        if(isNotHidden)
+        {
+            bufferPosition = transform.position;
             speed = 0;
             currentHideout.GetComponent<BoxCollider2D>().isTrigger = true;
             Debug.Log(bufferPosition);
-            gameObject.GetComponent<Transform>().position = currentHideout.gameObject.GetComponent<Transform>().position;
+            transform.position = currentHideout.gameObject.transform.position;
             hidden = true;
-            hiding = false;
+            isHiding = false;
         }
         else
         {
             Debug.Log(currentHideout);
             speed = speedBuffer;
-            gameObject.GetComponent<Transform>().position = bufferPosition;
+            transform.position = bufferPosition;
             currentHideout.GetComponent<BoxCollider2D>().isTrigger = false;
 
         }
 
     }
-    void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.tag == "hideout")
+        if(col.gameObject.CompareTag("hideout"))
         {
             //Debug.Log("HIDE" + col.gameObject.GetComponent<Transform>().position);
-            hiding = true;
+            isHiding = true;
             currentHideout = col.gameObject;
         }
 
 
     }
-    void OnTriggerExit2D(Collider2D col)
+    private void OnTriggerExit2D(Collider2D col)
     {
-        if(col.gameObject.tag == "hideout")
+        if(col.gameObject.CompareTag("hideout"))
         {
-            hiding = false;
-
+            isHiding = false;
         }
     }
 }
