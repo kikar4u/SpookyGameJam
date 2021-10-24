8d5ae835bd0e1a5b49c90f8d7a9712fd3a92e651
Sylve <sylve.renault@yahoo.fr>
2021-10-24 15:27:08 +0200

    [FX]Sound monster

---
+++ Assets/MonsterSoundGen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSoundGen : MonoBehaviour
+{
+    [Header("Audio")]
+    [SerializeField] private AudioSource audio;
+    [SerializeField] private List<AudioClip> walkingDefaultSounds = new List<AudioClip>();
+    // Start is called before the first frame update
+    public void GenWalkingSound(float soundMult)
+    {
+        List<AudioClip> clips = walkingDefaultSounds;
+        PlayAudio(clips[0], soundMult);
+    }
+    private void PlayAudio(AudioClip audioClip, float soundMult)
+    {
+        audio.volume = soundMult;
+        audio.clip = audioClip;
+        audio.pitch = Random.Range(-3.0f, 1.0f);
+        audio.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
